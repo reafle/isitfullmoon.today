7703ca641812dc1088ce25ec90231a16d15a30c6
reafle <reafle@gmail.com>
2022-02-03 14:08:43 +0200

    add google tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
+
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'G-HKC2GSZ570' })
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=Edge" />
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <meta name="author" content="" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
